9878be9470ebeef04fd904ca82eb08f89ab0cb4f
unknown <serhiiromanchuk@gmail.com>
2023-04-01 20:15:28 +0300

    implement MyJobService class

---
+++ app/src/main/java/com/example/services/MyJobService.kt
@@ -0,0 +1,51 @@
+package com.example.services
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyJobService : JobService() {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onStartJob")
+        scope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer: $i")
+            }
+            jobFinished(params, true)
+        }
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
+        log("onDestroy")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyService: $message")
+    }
+
+    companion object {
+        fun getIntent(context: Context): Intent {
+            return Intent(context, MyJobService::class.java)
+        }
+    }
+}
