bff9659d1b69c5c392d2c08b54db2fd9f1812822
unknown <serhiiromanchuk@gmail.com>
2023-04-03 17:03:29 +0300

    start MyJobService with enqueue method

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.services
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.app.job.JobService
+import android.app.job.JobWorkItem
 import android.content.ComponentName
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var page = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,11 +45,14 @@ class MainActivity : AppCompatActivity() {
             val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
                 .setRequiresCharging(true)
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
-                .setPersisted(true)
                 .build()
 
             val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
-            jobScheduler.schedule(jobInfo)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            }
         }
     }
 }
--- app/src/main/java/com/example/services/MyJobService.kt
+++ app/src/main/java/com/example/services/MyJobService.kt
@@ -4,6 +4,8 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.coroutines.*
 
@@ -18,12 +20,23 @@ class MyJobService : JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         log("onStartJob")
-        scope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer: $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            scope.launch {
+                var workItem = params?.dequeueWork()
+
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(params, true)
         }
         return true
     }
@@ -45,5 +58,12 @@ class MyJobService : JobService() {
 
     companion object {
         const val JOB_ID = 111
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
